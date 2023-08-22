b93ad7d6ebea4c633b21b105d739492f31344b33
jery FOTO <85230959+jery123@users.noreply.github.com>
2023-08-22 20:15:06 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <p>
+        We are going to build a dashboard for our Publisheveryday-PED project
+      </p>
+    </div>
+  );
+}
+
+export default App;
