71c395c7dbf6cb7c6860e271ab0bfcb802b84170
jery FOTO <85230959+jery123@users.noreply.github.com>
2023-10-27 07:28:23 +0530

    saave

--- src/pages/bloggers/Blogs/Create.jsx
+++ src/pages/bloggers/Blogs/Create.jsx
@@ -80,8 +80,8 @@ const Create = () => {
           <Inject services={[HtmlEditor, Toolbar, Image, Link, QuickToolbar]} />
         </RichTextEditorComponent>+
       </div>
-      <div class="justify-end">
-        <button type="button" onClick={submit} class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover-bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Save</button>
+      <div className  ="justify-end">
+        <button type="button" onClick={submit} className  ="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover-bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Save</button>
       </div>
     </div>
   );
--- src/pages/bloggers/Blogs/Update.jsx
+++ src/pages/bloggers/Blogs/Update.jsx
@@ -26,10 +26,10 @@ const Update = () => {
           <Inject services={[HtmlEditor, Toolbar, Image, Link, QuickToolbar]} />
         </RichTextEditorComponent>
       </div>
-      <div class="justify-end">
-      <button type="button" class="focus:outline-none text-white bg-purple-700 hover:bg-purple-800 focus:ring-4 focus:ring-purple-300 font-medium rounded-lg text-sm px-5 py-2.5 mb-2 dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-purple-900">Save</button>
+      <div className="justify-end">
+      <button type="button" className="focus:outline-none text-white bg-purple-700 hover:bg-purple-800 focus:ring-4 focus:ring-purple-300 font-medium rounded-lg text-sm px-5 py-2.5 mb-2 dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-purple-900">Save</button>
       <Link to='/blg/blogs'>
-        <button type="button" class="text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700">Cancel</button>
+        <button type="button" className="text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700">Cancel</button>
       </Link>
     </div>
     </div>
